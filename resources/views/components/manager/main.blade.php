07b1497b19da718bb1bb956a7225d5ea39eea05e
Muhamad Usri Yusron <muhamad.usri@mhs.itenas.ac.id>
2025-03-04 13:40:18 +0700

    Refactor orders status header for improved layout

--- resources/views/components/manager/main.blade.php
+++ resources/views/components/manager/main.blade.php
@@ -73,23 +73,7 @@
             <div class="card-body">
                 <div class="d-flex align-items-center">
                     <div>
-                        <h6 class="mb-0">Orders Status</h6>
-                    </div>
-                    <div class="dropdown ms-auto">
-                        <a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i
-                                class="bx bx-dots-horizontal-rounded font-22 text-option"></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="javascript:;">Action</a>
-                            </li>
-                            <li><a class="dropdown-item" href="javascript:;">Another action</a>
-                            </li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="javascript:;">Something else here</a>
-                            </li>
-                        </ul>
+                        <h6 class="mb-2">Orders Status</h6>
                     </div>
                 </div>
                 <div class="table-responsive">
